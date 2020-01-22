c0b88e704d9e0dfa1f3527c69c14002ca9889af2
Joris Menke <j.menke@getinteractive.nl>
2020-01-22 16:34:37 +0100

    changed to doom template

--- src/App.vue
+++ src/App.vue
@@ -1,73 +1,59 @@
 <template>
   <div id="app">
-    <h1>Cookies: {{cookie}}</h1>
-    <h2>Autoclickers: {{autoclicker}}</h2>
-    <h2>Grandma's: {{grandma}}</h2>
-    <br>
-    <button v-on:click="bakeCookie">Bake Cookies</button><br>
-    <button v-on:click="buyClicker">Buy Autoclicker ({{autoclickerPrice}} Cookies)</button><br>
-    <button v-on:click="buyGrandma">Buy Grandma ({{grandmaPrice}} Cookies)</button><br>
+    <h1>Demons Killed: {{demons}}</h1>
+    <h2>Fist level: {{fists}}</h2>
+    <button v-if="!disabled" v-on:click="killFirstDemon">Kill your first demon on accident.</button><br>
+    <button v-on:click="upgradeFists">Upgrade fists ({{fistCost}})</button>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'app',
-  data () {
-    return {
-      //main Cookies
-      cookie: 0,
-
-      //grandma
-      grandma: 0,
-      grandmaPrice: 200,
-
-      //autoclicker
-      autoclicker: 0,
-      autoclickerPrice: 50,
-    }
-  },
-  methods: {
-    bakeCookie: function() {
-      this.cookie++;
-    },
-    clickerBake: function (amount) {
-      this.cookie  += amount;
-    },
-    buyClicker: function () {
-      if (this.cookie >= this.autoclickerPrice) {
-        this.cookie = this.cookie - this.autoclickerPrice;
-        this.autoclicker++;
+  export default {
+    name: "app",
+    data(){
+      return{
+        disabled: false,
+        demons: 0,
+        fists: 0,
+        fistCost: 1,
+        fistCostMultiplier: 2,
       }
     },
-    buyGrandma: function () {
-      if (this.cookie >= this.grandmaPrice) {
-        this.cookie = this.cookie - this.grandmaPrice;
-        this.grandma++;
+    methods: {
+      killFirstDemon: function () {
+        this.demons++;
+        this.disabled = true;
+      },
+
+      killDemons: function (amount) {
+        this.demons = this.demons += amount;
+      },
+      upgradeFists: function () {
+        if (this.demons >= this.fistCost){
+          this.fists++;
+          this.demons = this.demons - this.fistCost;
+          this.fistCost = this.fistCost * this.fistCostMultiplier;
+        }
       }
     },
-    grandmaBake: function (amount) {
-      this.cookie += 10 * amount;
-    },
-  },
-  mounted() {
-    let that = this;
-    setInterval(function () {
-        if (that.autoclicker >= 1){
-          that.clickerBake(that.autoclicker);
+    mounted() {
+      let that = this;
+      setInterval(function () {
+        if (that.fists >= 1) {
+          that.killDemons(that.fists);
         }
-        if (that.grandma >= 1){
-          that.grandmaBake(that.grandma);
-        }
-        const title = "Cookies: " + that.cookie;
+
+
+        //title
+        const title = "Demons killed: " + that.demons;
         document.title = title;
-    }, 1000);
-  },
-}
+      }, 1000)
+    },
+  }
 </script>
 
-<style>
-body{
-  text-align: center;
-}
+<style scoped>
+  body{
+    text-align: center;
+  }
 </style>
